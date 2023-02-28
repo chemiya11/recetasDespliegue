28cfcb7eb45f3dff739e67c52b0f16a43f03227f
chemiya <jmlozanoolmedo@gmail.com>
2023-02-28 15:53:44 +0100

    nuevo

--- config/conexion.js
+++ config/conexion.js
@@ -1,11 +1,14 @@
 const mysql = require('mysql');
+//import {DB_HOST,DB_USER,DB_PASSWORD,DB_PORT,DB_NAME} from './config.js'
+
+const {DB_HOST,DB_NAME,DB_USER,DB_PASSWORD,DB_PORT}=require('./config')
 
 const conexion = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'password',
-    port:'3306',
-    database: 'aplicacion_recetas'
+    host: DB_HOST,
+    user: DB_USER,
+    password: DB_PASSWORD,
+    port:DB_PORT,
+    database: DB_NAME
 });//conecto con la base de datos
 
 conexion.connect((err)=>{
---
+++ config/config.js
@@ -0,0 +1,9 @@
+const DB_USER='root';
+ const DB_HOST='localhost';
+ const DB_PASSWORD='password';
+ const DB_NAME='recetasdb';
+ const DB_PORT=3306;
+module.exports={
+DB_HOST,DB_NAME,DB_PASSWORD,DB_PORT,DB_USER
+ }
+
